f4c8963e20b992e110dbb14f995a48a0215fc838
alaska1997 <lsendoya@unal.edu.co>
2022-06-19 19:00:47 -0500

    first step done

---
+++ main.js
@@ -0,0 +1,55 @@
+const URL = 'https://platzi-avo.vercel.app/api/avo';
+const baseUrl = 'https://platzi-avo.vercel.app'
+
+async function getData() {
+
+    const res = await fetch(URL);
+    const { data } = await res.json()
+
+    //initialize variables
+    let title, imagen, price, containerITem;
+
+    //create an array to add all div
+    const allElements = [];
+
+    data.forEach(element => {
+        //create a title
+        title = document.createElement('h2');
+        title.textContent = element.name;
+        title.className = "fs-4 text-success text-center";
+
+
+        //create a img
+        imagen = document.createElement('img');
+        imagen.src = `${baseUrl}${element.image}`;
+
+        //cretate a p =>price
+        price = document.createElement('p');
+        price.textContent = `$${ element.price}`;
+        price.className = "fs-5 text-dark text-center";
+
+        //create div container
+        containerITem = document.createElement('div');
+        containerITem.append(title, imagen, price);
+
+        //add elements to array
+        allElements.push(containerITem);
+
+    });
+    //add the div container principle
+    const container = document.createElement('div');
+    container.classList.add('container');
+    document.body.append(container);
+
+    //add className to containerItem
+    allElements.forEach(elem => {
+        elem.className = "d-flex justify-content-center m-2 flex-column border rounded bg-ligth shadow"
+    })
+
+
+
+    //add all elements to container
+    container.append(...allElements);
+}
+
+getData();
